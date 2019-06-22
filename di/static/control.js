108146fb805b8ff1ece1782d75501a840db68efd
Aman Bhargava <amanb2000@gmail.com>
2019-06-22 15:41:51 -0400

    upload schema with javascrip for post

---
+++ di/static/control.js
@@ -0,0 +1,44 @@
+function download(ID) {
+	$.post('/downloadML',   // url
+		{
+			'ID': ID // data to be submit
+		},
+		function(data) {// success callback
+			console.log("Data: " + data);
+		}
+	);
+}
+
+function vote(ID, value) {
+	$.post('/voteML',   // url
+		{
+			'ID': ID, 
+			'value': value
+		}, // data to be submit
+		function(data) {// success callback
+			console.log("Data: " + data);
+		}
+	);
+}
+
+function save(ID) {
+	$.post('/bookmarkML',   // url
+		{
+			'ID': ID, 
+		}, // data to be submit
+		function(data) {// success callback
+			console.log("Data: " + data);
+		}
+	);
+}
+
+function report(ID) {
+	$.post('/reportML',   // url
+		{
+			'ID': ID, 
+		}, // data to be submit
+		function(data) {// success callback
+			console.log("Data: " + data);
+		}
+	);
+}
