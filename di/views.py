3af05f4444e08ddc503d11dfdefda445e6f99ce9
Aman Bhargava <amanb2000@gmail.com>
2019-06-23 00:51:49 -0400

    Fixed Query

--- di/views.py
+++ di/views.py
@@ -6,7 +6,8 @@ def instantiate_home():
     # Get DB
     cursor = get_conn().cursor()
     # Make request
-    query = "SELECT post_id, title, descript, img, username FROM posts JOIN users ON users.user_id = posts.added_by FROM posts"
+    query = '''SELECT post_id, title, descript, img, username FROM posts 
+    			JOIN users ON users.user_id = posts.added_by'''
     # SELECT COLUMN1, COLUMN2 FROM TABLENAME
     cursor.execute(query)
     return render_template("schema1.html",
